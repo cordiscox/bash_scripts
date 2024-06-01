b4900b751d1e30469cd4efe838eb7b5752a20352
cordiscox <joaquin_cordisco@hotmail.com>
2024-06-01 05:12:57 +0000

    Update variable names

--- create_tar.sh
+++ create_tar.sh
@@ -2,7 +2,7 @@
 set -vx
 #---------------------------------------------------------------------------
 # 	Module Name			: create_tar.sh
-#   Module Description  : Script to tar files contained in a directory with nomenclature prd+fp+IDFILE+d
+#   Module DeSCRIPTion  : SCRIPT to tar files contained in a directory with nomenclature prd+fp+IDFILE+d
 #
 #							ex: SESSION: S4J06 --> s4jfp06d
 #							ex: SESSION: ERIX04 --> erifpX04d
@@ -14,40 +14,37 @@ set -vx
 PRDSPECIF=$(echo ${S_PROCEXE} | cut -c 1-3)
 prdspecif=$(echo ${PRDSPECIF} | tr "[:upper:]" "[:lower:]")
 fichier=$(echo ${S_CODSESS} | cut -c 4-)
-date_trait="$(date +%d-%b-%Y)"
-heur_trait="$(date +%T)"
-ficlog=$UNXLOG/"${date_trait}"-"${heur_trait}"-"${S_PROCEXE}".log
-script="create_tar.sh"
-coderetour=0
-name_folder=${prdspecif}"fp"${fichier}"d"
-name_tar=${nomfic}
+FICLOG=$UNXLOG/"$(date +%d-%b-%Y)"-"$(date +%T)"-"${S_PROCEXE}".log
+SCRIPT="create_tar.sh"
+FOLDER_NAME=${prdspecif}"fp"${fichier}"d"
+TAR_NAME=${nomfic}
 
 #-----
 #----- Start of the create_tar.sh job
 #-----
-echo "${S_PROCEXE}: Starting ${script} on $(date +%d-%b-%Y) at $(date +%T)" >> ${ficlog}
+echo "${S_PROCEXE}: Starting ${SCRIPT} on $(date +%d-%b-%Y) at $(date +%T)" >> ${FICLOG}
 
-verif_folder=$(ls -ltr $UNXENVOI/. | grep ${name_folder} | wc -l)
+verif_folder=$(ls -ltr $UNXENVOI/. | grep ${FOLDER_NAME} | wc -l)
 if [ $verif_folder -eq 0 ]; then
-    echo "The directory ${name_folder} does not exist" >> ${ficlog}
+    echo "The directory ${FOLDER_NAME} does not exist" >> ${FICLOG}
     exit 1
 else
-    echo "The directory ${name_folder} exists" >> ${ficlog}
-    verif_fic=$(ls $UNXENVOI/${name_folder}/. | wc -l)
+    echo "The directory ${FOLDER_NAME} exists" >> ${FICLOG}
+    verif_fic=$(ls $UNXENVOI/${FOLDER_NAME}/. | wc -l)
     if [ $verif_fic -ne 0 ]; then
-        echo "The directory ${name_folder} is not empty" >> ${ficlog}
-        cd $UNXENVOI/${name_folder}
-        tar -vcf $name_tar *
+        echo "The directory ${FOLDER_NAME} is not empty" >> ${FICLOG}
+        cd $UNXENVOI/${FOLDER_NAME}
+        tar -vcf $TAR_NAME *
         if [ $? -ne 0 ]; then
-            echo "The .tar could not be created" >> ${ficlog}
+            echo "The .tar could not be created" >> ${FICLOG}
         else
-            chmod 777 $name_tar
-            mv $name_tar $UNXTMP
-            echo "The .tar has been created and placed in the tmp directory" >> ${ficlog}
+            chmod 777 $TAR_NAME
+            mv $TAR_NAME $UNXTMP
+            echo "The .tar has been created and placed in the tmp directory" >> ${FICLOG}
             rm *
         fi
     else
-        echo "The directory ${name_folder} is empty" >> ${ficlog}
+        echo "The directory ${FOLDER_NAME} is empty" >> ${FICLOG}
     fi
 fi
  
